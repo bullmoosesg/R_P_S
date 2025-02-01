c5831cb0c12a4bc38a6ae57bdab359ba755effc2
bullmoosesg <bullmoosesg@gmail.com>
2025-01-31 23:34:28 -0500

    branch created to add ui
    rewrote code, added html and css

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
+const rock = document.querySelector('#rock');
+const scissors = document.querySelector("#scissors");
+const paper = document.querySelector("#paper");
+const para = document.querySelector("#feedback");
+const table = document.querySelector("table");
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+const usrButtons = document.querySelectorAll(".selection");
+const reset = document.querySelector("#reset");
+const logs = document.querySelector("tbody");
 
-function getHumanChoice(){
-    let validchoices = ["rock", "paper", "scissors"];
-    let input = prompt("Rock, Paper, Scissors?").trim().toLowerCase();
-    if (input === "scissor") {
-        input = "scissors"
-    } 
 
-while(validchoices.indexOf(input) === -1){
-    input = prompt("Not quite, try again").trim().toLowerCase();
-}
-return input
+reset.addEventListener("click", ()=>{
+    humanScore = computerScore  = 0;
+    round = 0;
+    logs.innerHTML = '';
+    para.innerHTML= "Clean Slate! Its 0:0"
+    
 
-}
+})
 
-function playGame() {
+usrButtons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        playRound(button.id)
+    })
+})
 
-let humanScore = 0
-let computerScore = 0
 
-    function playRound(humanChoice,computerChoice){
+   function compLoop(){
+         switch(Math.floor(Math.random() * 3)) {
+               case 0:
+                   return "rock";
+                   break;
+                case 1:
+                   return "paper";
+                   break;
+                case 2:
+                   return "scissors";
+                   break;
+         };
+        };
+
+    function playRound(humanChoice){
+        let computerChoice = compLoop();
         let result = (humanChoice+"/"+computerChoice);
         let resultlog = null;
-
+        let winnerText = ''
+    
         switch(result){
             case "paper/paper":
             case "rock/rock":
@@ -32,41 +57,43 @@ let computerScore = 0
             case "rock/scissors":
             case "scissors/paper":
                 humanScore ++;
-                resultlog = `You win! You entered ${humanChoice} and the computer selected ${computerChoice}. 
-                The score is now You:${humanScore}, the Robot:${computerScore}.`;
+                winnerText = "You";
+                round ++;
+                resultlog = `You win! You entered ${humanChoice} and the computer selected ${computerChoice}.`;
                 break;
             case "rock/paper":
             case "scissors/rock":
             case "paper/scissors":
                 computerScore ++;
-                resultlog = `You lost, you entered ${humanChoice} and the computer selected ${computerChoice}. 
-                The score is now You:${humanScore}, the Robot:${computerScore}.`;
+                winnerText = " the computer";
+                round ++;
+                resultlog = `You lost, you entered ${humanChoice} and the computer selected ${computerChoice}.`;
                 break;
         }
+    
+        if(computerChoice != humanChoice){
+            let row = table.insertRow(-1);
+            row.setAttribute("class","logs");
+            let rnd = row.insertCell(0);
+            let humanCH = row.insertCell(1);
+            let compCH = row.insertCell(2);
+            let winner = row.insertCell(3);
+            let score = row.insertCell(4);
+            rnd.innerHTML = round;
+            compCH.innerHTML = computerChoice;
+            humanCH.innerHTML = humanChoice;
+            score.innerHTML = `${humanScore} : ${computerScore}`;
+            winner.innerHTML = winnerText;
+        };
 
-    console.log(resultlog);
-    }
-
-    for (let i = 0; i < 4; i++){
-        function compLoop(){
-        switch(Math.floor(Math.random() * 3)) {
-            case 0:
-                return compLoop ="rock";
-                break;
-            case 1:
-                return compLoop ="paper";
-                break;
-            case 2:
-                return compLoop ="scissors";
-                break;
-        }
-        }
-        console.log(`Round ${i+1} Make your pick`);
-        playRound(getHumanChoice(),compLoop());
-    }
-
-    console.log(`Game Over. You: ${humanScore}. Computer:${computerScore}`);
-}
+        if(round == 5){
+            para.innerHTML = `And that's the game, the winner was ${winnerText} with a score of ${Math.max(computerScore,humanScore)}. Click the reset botton to play again.`
+        } else{
+        para.innerHTML = resultlog;
+        };
+    
+    };
 
 
-playGame();
+
+
